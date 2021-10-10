b68360f0af5681d8cb48795f6d4f0593fe51815a
Sarmin  Lipsy <60995247+Lipsy-gif@users.noreply.github.com>
2021-10-10 21:08:47 +0600

    Add files via upload

---
+++ Lab3/changephoto.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Profile Picture</title>
+    
+</head>
+<body>
+    <div class="img">
+        <form  class="upload-form">
+            <h2>Profile Picture</h2>
+            <img src="img.png" alt="picture">
+            <input class="upload-file"  type="file" accept="image/png,image/jpeg"><br><br>
+            <hr>
+            <input type=submit>
+        </form>
+    </div>
+</body>
+</html>
