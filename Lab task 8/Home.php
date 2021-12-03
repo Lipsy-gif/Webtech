09d07ac89bc79777967ef2aa4a8907afcf59394f
Sarmin  Lipsy <60995247+Lipsy-gif@users.noreply.github.com>
2021-12-04 02:27:10 +0600

    Add files via upload

---
+++ Lab task 8/Home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Tinker </title>
+  </head>
+ 
+ <body>
+			<h1 align='center' style="font-family:Algerian">Tinker</h1>
+ <table style="width:100%; border:1px solid  #b3b3b3">
+    <tr  style="background-color:#04AA6D;">
+        <th style="border: 5px solid #04AA6D;"><a style="text-decoration:none; color:#fff;" href="adminlogin.php">Admin</a></th>
+        <th style="border: 5px solid #04AA6D;"><a style="text-decoration:none; color:#fff;" href="StoreOwnerlogin.php">Store Owner</a></th>
+        <th style="border: 5px solid #04AA6D;"><a style="text-decoration:none; color:#fff;" href="login.php">Customer</a></th>
+        <th style="border: 5px solid #04AA6D;"><a style="text-decoration:none; color:#fff;" href="Employee.php">Employee</a></th>
+    </tr>
+    </table>
+<br>
+        <h1 align='center' style="font-family:Poor Richard">"Find All in a Single Site "</h1>
+        <center><a href="Home.php">
+        <h3 align='center' style="font-family:Poor Richard">STAY HOME , STAY SAFE</h3>   
+       <div style="background-color: white;">
+    <center><font size="3" face="arial" >
+        <br><br>
+        <a href="aboutUs.php" class='about'>About Us</a><br>
+        <br>
+        <a href="contactUs.php" class='contact'>Contact </a><br>.
+        <br>
+        <br>
+        <br>
+        </font></center>
+    </div> 
+  </body>
+    
+</html>
---
+++ Lab task 8/cart.php
@@ -0,0 +1,68 @@
+<?php 
+     include 'showLogout.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View cart</title>
+    <style>
+    table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    padding: 5px;
+    }
+    table{
+        margin: 50px;
+    }
+    </style>
+</head>
+<body>
+    <?php
+       if(isset($_POST["order"]))
+       {
+        header("location:payment.php");
+       } 
+    ?>
+    
+    <center>
+        <h2>Add cart</h2>
+        <form method="post"> 
+            <table>
+                <tr>
+                <th>Name</th>
+                <th>Price</th>
+                </tr>
+                <?php
+                $dtls = file_get_contents('addCart.json');
+                $dtlsok = json_decode($dtls);
+                    $total_price=0;
+                    foreach($dtlsok as $ok)
+                    {
+                    
+                        $name=$ok->name;
+                        $price=$ok->price;
+                        $total_price+=(int)$price;
+                        echo "<tr>";
+                        echo "<td>$name</td>";
+                        echo "<td>$price</td>";
+                        echo "</tr>";
+                        
+                        
+                        
+                    }
+                    echo "<tr>";
+                    echo "<td>Total Price</td>";
+                    echo "<td>$total_price</td>";
+                    echo "</tr>";
+                
+                ?>
+            </table>
+            <input type="submit" name="order" value="Buy Now"  />
+        </form>
+    </center>
+</body>
+</html>
+
